224210fed4c6a0fb47563ea33afd7022136df97a
Naveen Dangwal <naveendangwal22@gmail.com>
2025-12-10 12:11:27 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv');
+dotenv.config();
+const cors=require('cors');
+const connectDB=require('./config/db')
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('this is the paytm payment server');
+});
+
+const PORT=process.env.PORT;
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on PORT ${PORT}`);
+    });
+})
+
